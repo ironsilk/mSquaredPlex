a77fd4fc549e6784b30c447cbc257b7b06ffc631
ryouthere <mihai.vlad6@gmail.com>
2021-09-04 15:55:07 +0300

    fixing all kinds of shit.

--- torr_service/change_transmission_settings.sh
+++ torr_service/change_transmission_settings.sh
@@ -2,4 +2,5 @@
 
 jq '."script-torrent-done-enabled" = true' /defaults/settings.json
 jq '."script-torrent-done-filename" = "/torr_finished_routine.sh"' /defaults/settings.json
+jq '."incomplete-dir" = ""' /defaults/settings.json
 jq '."incomplete-dir-enabled" = "false"' /defaults/settings.json
